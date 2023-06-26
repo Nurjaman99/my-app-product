7c5019ee9e6f45c73ab13eece0276dcdaebb8878
Nurjaman99 <137714423+Nurjaman99@users.noreply.github.com>
2023-06-26 14:19:14 +0700

    Delete src directory

--- src/component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <>
-            <div className={'flex h-20 bg-sky-900 py-8 px-32 justify-between'}>
-            </div>
-        </>
-    )
-}
-export default Footer;
